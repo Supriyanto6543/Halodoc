bf46da1f05190a6798d37ad66780a9eb6f87c03c
Supriyanto <supriyanto6543@gmail.com>
2020-06-17 06:13:43 +0700

    initial commits

--- app/src/main/java/com/halodoc/medical/MainActivity.java
+++ app/src/main/java/com/halodoc/medical/MainActivity.java
@@ -1,8 +1,16 @@
 package com.halodoc.medical;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.luseen.spacenavigation.SpaceItem;
 import com.luseen.spacenavigation.SpaceNavigationView;
@@ -11,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     SpaceNavigationView spaceNavigationView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +28,10 @@ public class MainActivity extends AppCompatActivity {
         spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
         spaceNavigationView.addSpaceItem(new SpaceItem("PROFILE", R.drawable.ic_home));
-        spaceNavigationView.addSpaceItem(new SpaceItem("HOME", R.drawable.ic_home));
         spaceNavigationView.addSpaceItem(new SpaceItem("LAINNYA", R.drawable.ic_home));
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
     }
+
 }
