5f46323968cc185006cfbe4796323c7bd0aa8929
Supriyanto <supriyanto6543@gmail.com>
2020-06-16 22:51:41 +0700

    initial commits

--- app/src/main/java/com/halodoc/medical/MainActivity.java
+++ app/src/main/java/com/halodoc/medical/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    SpaceNavigationView spaceNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //set
+        spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
+        spaceNavigationView.addSpaceItem(new SpaceItem("PROFILE", R.drawable.ic_home));
+        spaceNavigationView.addSpaceItem(new SpaceItem("HOME", R.drawable.ic_home));
+        spaceNavigationView.addSpaceItem(new SpaceItem("LAINNYA", R.drawable.ic_home));
     }
 }
