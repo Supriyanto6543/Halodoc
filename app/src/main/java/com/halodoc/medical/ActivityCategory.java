c58ba95d64dc3d8d1efa3de1897fa5ddba5a91e5
Supriyanto <supriyanto6543@gmail.com>
2020-06-18 06:31:35 +0700

    initial commits days 2

--- app/src/main/java/com/halodoc/medical/ActivityCategory.java
+++ app/src/main/java/com/halodoc/medical/ActivityCategory.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ActivityCategory extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-
+        toolbar = findViewById(R.id.toolbar);
     }
 }
