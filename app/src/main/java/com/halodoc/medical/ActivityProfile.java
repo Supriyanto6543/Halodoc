2f8cdca374d0103c9eb6eb37e45c60ef8f9a1cec
Supriyanto <supriyanto6543@gmail.com>
2020-06-19 05:54:58 +0700

    initial commits days 3

--- app/src/main/java/com/halodoc/medical/ActivityProfile.java
+++ app/src/main/java/com/halodoc/medical/ActivityProfile.java
@@ -1,16 +1,58 @@
 package com.halodoc.medical;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ActivityProfile extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView name, ttl, hp, email, berat, tiggi;
+    String names, ttls, hps, emails, berats, tinggis;
+    Button chat;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.toolbar);
+        chat = findViewById(R.id.chat);
+        name = findViewById(R.id.name);
+        ttl = findViewById(R.id.ttl);
+        hp = findViewById(R.id.hp);
+        email = findViewById(R.id.email);
+        berat = findViewById(R.id.berat);
+        tiggi = findViewById(R.id.tinggi);
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                names = name.getText().toString();
+                ttls = ttl.getText().toString();
+                hps = hp.getText().toString();
+                emails = email.getText().toString();
+                berats = berat.getText().toString();
+                tinggis = tiggi.getText().toString();
+
+                if (names.isEmpty() && ttls.isEmpty() && berats.isEmpty() &&  hps.isEmpty() && emails.isEmpty() && tinggis.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Silahkan lengkapi semua data", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "BERHASIL EDIT DATA", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Lengkapi Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 }
--- app/src/main/java/com/halodoc/medical/fragment/FragmentLainnya.java
+++ app/src/main/java/com/halodoc/medical/fragment/FragmentLainnya.java
@@ -7,16 +7,24 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.halodoc.medical.R;
 
 public class FragmentLainnya extends Fragment {
 
+    Toolbar toolbar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lainnya, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Lainnya");
 
         return view;
     }
