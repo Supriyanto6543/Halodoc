df5192427b753c375b6705ee770f1b951ecd634d
Supriyanto <supriyanto6543@gmail.com>
2020-09-05 16:34:02 +0700

    adding chat

--- app/src/main/java/com/halodoc/medical/ActivityTransfer.java
+++ app/src/main/java/com/halodoc/medical/ActivityTransfer.java
@@ -1,5 +1,8 @@
 package com.halodoc.medical;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -7,4 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
  * Copyright (c) 2020 Murottal Pro . All rights reserved.
  */
 public class ActivityTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+    }
 }
